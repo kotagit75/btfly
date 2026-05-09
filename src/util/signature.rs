93ea8632e10f3eef97e535c733c734eac4fb93b5
kotagit75 <unity75lets5@gmail.com>
2026-05-10 08:32:05 +0900

    Fixed a bug where the signature verification always returned true

--- src/util/signature.rs
+++ src/util/signature.rs
@@ -19,6 +19,9 @@ pub fn sign(data: &[u8], sk: SK) -> Result<Signature, ErrorStack> {
 
 pub fn verify(data: &[u8], pk: PK, signature: Signature) -> bool {
     Verifier::new(MessageDigest::sha256(), &pk.key())
-        .and_then(|mut verifyer| verifyer.update(data).map(|_| verifyer.verify(&signature)))
-        .is_ok()
+        .and_then(|mut verifyer| {
+            verifyer.update(data)?;
+            verifyer.verify(&signature)
+        })
+        .unwrap_or(false)
 }
